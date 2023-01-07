50950b0443f26683b419b20ddf0e320f196ceab8
ashikmahmud <ashikmamud187@gmail.com>
2023-01-08 04:31:49 +0600

    Done to design dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,25 +1,89 @@
+import Image from "next/image";
 import DashboardLayout from "../../src/layout/DashboardLayout/dashboardLayout";
 
 type Props = {};
 
 const Dashboard = (props: Props) => {
   return (
-    <>
-      <p>
-        this is Lorem ipsum dolor sit, amet consectetur adipisicing elit. In
-        aperiam rem maiores officia tenetur sunt voluptatum alias, accusamus
-        repudiandae corporis dolor sapiente eveniet veniam commodi praesentium
-        iusto officiis quisquam voluptas! Error culpa saepe dolores dolor sint
-        commodi incidunt omnis repellendus eligendi minus laborum, minima
-        numquam consequuntur, accusamus deserunt. In cum excepturi qui
-        dignissimos ipsa iste rem? Mollitia officiis animi dolore laudantium,
-        eos nesciunt, aliquam perferendis cupiditate porro qui deleniti dolorem
-        ab quos consectetur sequi ratione nihil reprehenderit quaerat repellat
-        distinctio illum. Corporis, quam blanditiis dignissimos libero dolores
-        illum repudiandae ipsum consequatur fugiat facilis dolor rerum delectus
-        quae, qui maiores sunt.
-      </p>
-    </>
+    <div>
+      <h1 className="text-2xl font-bold text-violet-500">Dashboard</h1>
+
+      {/* overview */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
+        <div className="bg-white p-4 rounded-md shadow-md">
+          <h1 className="text-xl font-bold text-violet-500">Overview</h1>
+          <div className="flex flex-col gap-2 mt-4">
+            <div className="flex justify-between">
+              <p className="text-sm font-medium text-gray-500">
+                Total Products
+              </p>
+              <p className="text-sm font-medium text-gray-500">10</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-sm font-medium text-gray-500">Total Orders</p>
+              <p className="text-sm font-medium text-gray-500">10</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-sm font-medium text-gray-500">Total Users</p>
+              <p className="text-sm font-medium text-gray-500">10</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-sm font-medium text-gray-500">Total Revenue</p>
+              <p className="text-sm font-medium text-gray-500">10</p>
+            </div>
+          </div>
+        </div>
+        {/* end overview */}
+
+        {/* recent added product */}
+        <div className="bg-white p-4 rounded-md shadow-md">
+          <h1 className="text-xl font-bold text-violet-500">
+            Recent Added Products
+          </h1>
+          <div className="flex flex-col gap-2 mt-4">
+            <div className="flex justify-between">
+              <p className="text-sm font-medium text-gray-500">Product Name</p>
+              <p className="text-sm font-medium text-gray-500">Price</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-sm font-medium text-gray-500">Product Name</p>
+              <p className="text-sm font-medium text-gray-500">Price</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-sm font-medium text-gray-500">Product Name</p>
+              <p className="text-sm font-medium text-gray-500">Price</p>
+            </div>
+
+            <div className="flex justify-between">
+              <p className="text-sm font-medium text-gray-500">Product Name</p>
+              <p className="text-sm font-medium text-gray-500">Price</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* end recent added product */}
+
+      {/* recent added users */}
+      <div className="bg-white p-4 rounded-md shadow-md mt-5">
+        <h1 className="text-xl font-bold text-violet-500">
+          Recent Added Users
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 my-6">
+          {/* card */}
+          <div className="card bg-white shadow p-6 rounded text-center flex items-center justify-center flex-col">
+            <Image src="/Avatar.png" width={50} height={50} alt="avatar" />
+            <div className="card-body">
+              <h2 className="card-title text-xl font-bold">John Doe</h2>
+              <p className="card-text">Admin</p>
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
