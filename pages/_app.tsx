c82176ebc2555bd778bcd64c2c0cb30ec2126ada
ashikmahmud <ashikmamud187@gmail.com>
2023-01-08 22:29:59 +0600

    added config for redux-toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
 import GlobalLayout from "../src/layout/GlobalLayout/GlobalLayout";
 import "../styles/globals.css";
 import "../styles/style.scss";
@@ -8,5 +10,9 @@ export default function App({ Component, pageProps }: AppProps | any) {
     Component.getLayout ||
     ((page: React.ReactNode) => <GlobalLayout>{page}</GlobalLayout>);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
