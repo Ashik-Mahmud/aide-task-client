761be32466e647468ab57a7ccdc5b18d09eaa1de
ashikmahmud <ashikmamud187@gmail.com>
2023-01-09 07:15:40 +0600

    done live url for backend

--- config/config.ts
+++ config/config.ts
@@ -1,1 +1,2 @@
-export const server_url = "http://localhost:5000/api";
+// export const server_url = "http://localhost:5000/api";
+export const server_url = "https://aide-task-server-v1.vercel.app/api";
